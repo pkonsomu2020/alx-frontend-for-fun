9553c0911fbe95076450dec2f6e5cff18f19ccfd
pkonsomu2020 <pkonsomu2020@gmail.com>
2023-08-09 00:31:31 -0700

    Updated file

--- markdown2html.py
+++ markdown2html.py
@@ -1,77 +1,50 @@
-#!/usr/bin/python3.8
+#!/usr/bin/python3
+"""
+A script that converts Markdown to HTML.
+"""
 
-import markdown
 import sys
+import os
 import re
-import hashlib
 
-def convert_to_html(markdown_file):
-    with open(markdown_file, 'r') as file:
-        markdown_content = file.read()
-
-        # Regular expression to match bold and emphasis syntax
-        bold_pattern = re.compile(r'\*\*(.*?)\*\*')
-        emphasis_pattern = re.compile(r'__(.*?)__')
-        md5_pattern = re.compile(r'\[\[(.*?)\]\]')
-        remove_c_pattern = re.compile(r'\(\((.*?)\)\)')
-        markdown_lines = markdown_content.split('\n')
-        html_lines = []
-
-        in_list = False
-
-        for line in markdown_lines:
-            # Check for list items
-            list_match = re.match(r'^\s*-\s+(.*)$', line)
-            if list_match:
-                if not in_list:
-                    html_lines.append('<ul>')
-                    in_list = True
-                list_item = list_match.group(1)
-                # Check for bold and emphasis syntax in list item
-                list_item = bold_pattern.sub(r'<b>\1</b>', list_item)
-                list_item = emphasis_pattern.sub(r'<em>\1</em>', list_item)
-                # Check for MD5 syntax in list item
-                list_item = md5_pattern.sub(lambda x: hashlib.md5(x.group(1).encode()).hexdigest(), list_item.lower())
-                # Check for remove 'c' syntax in list item
-                list_item = remove_c_pattern.sub(lambda x: x.group(1).replace('c', ''), list_item)
-                html_line = f"<li>{list_item}</li>"
-                html_lines.append(html_line)
-            else:
-                # Check for paragraph lines
-                para_match = re.match(r'^\s*(.*?)$', line)
-                if para_match:
-                    if in_list:
-                        html_lines.append('</ul>')
-                        in_list = False
-                    para_text = para_match.group(1)
-                    # Check for bold and emphasis syntax in paragraph
-                    para_text = bold_pattern.sub(r'<b>\1</b>', para_text)
-                    para_text = emphasis_pattern.sub(r'<em>\1</em>', para_text)
-                    # Check for MD5 syntax in paragraph
-                    para_text = md5_pattern.sub(lambda x: hashlib.md5(x.group(1).encode()).hexdigest(), para_text.lower())
-                    # Check for remove 'c' syntax in paragraph
-                    para_text = remove_c_pattern.sub(lambda x: x.group(1).replace('c', ''), para_text)
-                    html_line = f"<p>{para_text}</p>"
-                    html_lines.append(html_line)
-
-        if in_list:
-            html_lines.append('</ul>')
-
-        html_content = '\n'.join(html_lines)
-        return html_content
-
-def save_html_file(html_content, output_file):
-    with open(output_file, 'w') as file:
-        file.write(html_content)
-
-if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Usage: python markdown_to_html.py input_file output_file")
+def convert_markdown_to_html(input_file, output_file):
+    """
+    Converts a Markdown file to HTML and writes the output to a file.
+    """
+    # Check that the Markdown file exists and is a file
+    if not (os.path.exists(input_file) and os.path.isfile(input_file)):
+        print(f"Missing {input_file}", file=sys.stderr)
         sys.exit(1)
 
+    # Read the Markdown file and convert it to HTML
+    with open(input_file, encoding="utf-8") as f:
+        html_lines = []
+        for line in f:
+            # Check for Markdown headings
+            match = re.match(r"^(#+) (.*)$", line)
+            if match:
+                heading_level = len(match.group(1))
+                heading_text = match.group(2)
+                html_lines.append(f"<h{heading_level}>{heading_text}</h{heading_level}>")
+            else:
+                html_lines.append(line.rstrip())
+
+    # Write the HTML output to a file
+    with open(output_file, "w", encoding="utf-8") as f:
+        f.write("\n".join(html_lines))
+
+if __name__ == "__main__":
+    # Check that the correct number of arguments were provided
+    if len(sys.argv) != 3:
+        print("Usage: ./markdown2html.py <input_file> <output_file>", file=sys.stderr)
+        sys.exit(1)
+
+    # Get the input and output file names from the command-line arguments
     input_file = sys.argv[1]
     output_file = sys.argv[2]
 
-    html_content = convert_to_html(input_file)
-    save_html_file(html_content, output_file)
+    # Convert the Markdown file to HTML and write the output to a file
+    convert_markdown_to_html(input_file, output_file)
 
+    # Exit with a successful status code
+    sys.exit(0)
